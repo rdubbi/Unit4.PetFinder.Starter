1946b1ece6d034cce0f57f2403a06762edc0f07b
Roy Watson-Badell <rwatsonbadell@roys-mbp.mynetworksettings.com>
2024-02-14 14:47:55 -0500

    working endpoints and static html

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
 // import the pets array from data.js
-const pets = require('./data');
+const pets = require("./data");
 
 // init express app
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = 8080;
 
-// GET - / - returns homepage
-app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
+// initiate the path package to serve up index html on the front end
+const path = require("path");
 
+// GET - / - returns homepage -- WORKING
+// localhost:8080
+
+app.get("/", (req, res) => {
+  // serve up the public folder as static index.html file
+
+  // res.sendFile a file with dirname and index.html
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-// hello world route
-app.get('/api', (req, res) => {
-    res.send('Hello World!');
+// GET - hello world route -- WORKING
+// localhost:8080/api
+
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
 });
 
-// get all pets from the database
-app.get('/api/v1/pets', (req, res) => {
-    // send the pets array as a response
+// GET - get all pets from the database -- WORKING
+// localhost:8080/api/v1/pets
 
+app.get("/api/v1/pets", (req, res) => {
+  // send the pets array as a response
+  // response a 200 status code and a JSON of pets
+  res.status(200).json(pets);
 });
 
-// get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
-    // get the owner from the request
+// GET - get pet by owner with query string -- WORKING
+// localhost:8080/api/v1/pets/owner?owner=John
 
+app.get("/api/v1/pets/owner", (req, res) => {
+  // get the owner from the request
+  // create a variable the holds the query of owner
+  const owner = req.query.owner;
+  // find the pet in the pets array
 
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
+  //   default is .find but .filter is more appropriate because the data shows that owner John owns both Fido
+  // and Rover. With .filter we get both when entering query John!
+  const pet = pets.filter((pet) => pet.owner === owner);
 
-    // send the pet as a response
-
+  // send the pet as a response
+  res.json(pet);
 });
 
-// get pet by name
-app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
+// GET - get pet by name -- WORKING
+// localhost:8080/api/v1/pets/Rover
 
+app.get("/api/v1/pets/:name", (req, res) => {
+  // get the name from the request
+  // create a variable the holds the query of name
+  const name = req.params.name;
+  // find the pet in the pets array
+  const pet = pets.find((pet) => pet.name === name);
 
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
-
-    // send the pet as a response
-
+  // send the pet as a response
+  res.json(pet);
 });
 
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
+  console.log("Server is listening on port " + PORT);
 });
 
 module.exports = app;
